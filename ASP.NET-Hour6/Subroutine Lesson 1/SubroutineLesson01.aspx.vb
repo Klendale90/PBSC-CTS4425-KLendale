5455f28ce8ffd81da75a4a05370411914d12f26d
Klendale90 <kerialendale@gmail.com>
2025-06-06 13:52:13 -0400

    Add files via upload

---
+++ ASP.NET-Hour6/Subroutine Lesson 1/SubroutineLesson01.aspx.vb
@@ -0,0 +1,22 @@
+﻿
+Partial Class SubroutineLesson01
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Output.Text = String.Empty
+
+        ShowWelcomeMessage()
+    End Sub
+
+    Protected Sub DisplayMessage_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles DisplayMessage.Click
+        ShowWelcomeMessage()
+    End Sub
+
+    Private Sub ShowWelcomeMessage()
+        Dim welcomeMessageIndex As Integer
+        For welcomeMessageIndex = 1 To 4
+            Output.Text &= "Welcome to my Website<br />"
+        Next
+    End Sub
+
+End Class
---
+++ ASP.NET-Hour6/Time Appropriate Message/TimeAppropriateMessage.aspx.vb
@@ -0,0 +1,18 @@
+﻿
+Partial Class TimeAppropriateMessage
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If DateTime.Now.Hour >= 6 And DateTime.Now.Hour < 12 Then
+            Message.Text = "Good morning."
+        End If
+
+        If DateTime.Now.Hour >= 12 And DateTime.Now.Hour < 17 Then
+            Message.Text = "Good afternoon."
+        End If
+
+        If DateTime.Now.Hour > 17 Or DateTime.Now.Hour < 6 Then
+            Message.Text = "Good evening."
+        End If
+    End Sub
+End Class
